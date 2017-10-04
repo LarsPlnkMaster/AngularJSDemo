47a76070c544387635dbd2b3d7b5ac1a4581b6d3
Lars Segelstrand <lars.segelstrand@sogeti.se>
2017-10-04 17:20:22 +0200

    testing

---
+++ directives/contactCard.js
@@ -0,0 +1,16 @@
+﻿angular.module('app.directives.contactCard', [])
+    .directive('contactCard', function () {
+        return {
+            restrict: 'E',
+            scope: {
+                cust:'=data'
+            },
+            replace:true,
+            templateUrl:"templates/directives/contactCard.html",
+            controller: function ($scope) {
+                console.log($scope.cust);
+            },
+            
+        };
+    });
+    
